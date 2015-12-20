4f1f6ee6df81e3de56352ec37dffa38f8c0518bf
Dhaval Asodariya <dhavalasodariya@outlook.com>
2015-12-20 15:27:13 +0530

    Create JQHelper.0.0.1.js

---
+++ JQHelper.0.0.1.js
@@ -0,0 +1,49 @@
+
+//============================================  val/html
+
+// GET/SET VALUES
+function val(ObjectName, Values)
+{    
+	if(Values == "")
+	{
+		return $(ObjectName).val();
+	}
+	else{
+	    $(ObjectName).val(Values);		
+	}	
+}
+
+// GET/SET HTML
+function html(ObjectName, Html) {
+	if(Html == "")
+	{
+		return $(ObjectName).html();
+	}
+	else{
+		$(ObjectName).append(Html);
+	}	
+}
+
+//=========================================== hide/show
+
+// Hide in document
+function hide(ObjectName){
+	$(ObjectName).hide();
+}
+
+// Show in document
+function show(ObjectName){
+	$(ObjectName).show();
+}
+
+//=========================================== click/load
+
+// Object Click Event
+function click(ObjectName, callfunction){
+	$( ObjectName).click(callfunction);
+}
+
+// Object Load Event
+function load(ObjectName,URL){
+	$( ObjectName).load(URL);
+}
