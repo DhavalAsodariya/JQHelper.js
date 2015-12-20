22510301a2eab6c0b4ab350c305d68d205c4c8ab
Dhaval Asodariya <dhavalasodariya@outlook.com>
2015-12-20 16:01:04 +0530

    Update JQHelper.0.0.1.js

--- JQHelper.0.0.1.js
+++ JQHelper.0.0.1.js
@@ -2,48 +2,25 @@
 //============================================  val/html
 
 // GET/SET VALUES
-function val(ObjectName, Values)
-{    
-	if(Values == " ")
-	{
-		return $(ObjectName).val();
-	}
-	else{
-	    $(ObjectName).val(Values);		
-	}	
-}
+function getval(ObjectName){return $(ObjectName).val();}
+function setval(ObjectName, Values){$(ObjectName).val(Values);}
 
 // GET/SET HTML
-function html(ObjectName, Html) {
-	if(Html == " "
-	{
-		return $(ObjectName).html();
-	}
-	else{
-		$(ObjectName).append(Html);
-	}	
-}
+function gethtml(ObjectName) {return $(ObjectName).html();}
+function sethtml(ObjectName, Html) {$(ObjectName).append(Html);}
 
 //=========================================== hide/show
 
 // Hide in document
-function hide(ObjectName){
-	$(ObjectName).hide();
-}
+function hide(ObjectName){$(ObjectName).hide();}
 
 // Show in document
-function show(ObjectName){
-	$(ObjectName).show();
-}
+function show(ObjectName){$(ObjectName).show();}
 
 //=========================================== click/load
 
 // Object Click Event
-function click(ObjectName, callfunction){
-	$( ObjectName).click(callfunction);
-}
+function click(ObjectName, callfunction){$( ObjectName).click(callfunction);}
 
 // Object Load Event
-function load(ObjectName,URL){
-	$( ObjectName).load(URL);
-}
+function load(ObjectName,URL){$( ObjectName).load(URL);}
